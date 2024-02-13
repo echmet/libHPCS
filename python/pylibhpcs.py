4fd3e7d1477570dcfba8043552ed66a5f51a87cb
Michal Malý <madcatxster@devoid-pointer.net>
2024-02-13 22:52:27 +0100

    Expose Python-friendly API from the wrapper instead of C types and functions

--- python/pylibhpcs.py
+++ python/pylibhpcs.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import platform
 from ctypes import (
     c_uint8, c_uint16, c_uint32, c_int,
@@ -8,6 +9,8 @@ from ctypes import (
     Structure, POINTER,
     CDLL
 )
+from enum import IntEnum
+from typing import Dict, List, Optional, Tuple
 
 # Load the library based on the operating system
 if platform.system() == 'Windows':
@@ -19,6 +22,16 @@ else:
     lib_name = './libHPCS.so'
 
 libhpcs = CDLL(lib_name)
+
+
+class HPCSError(Exception):
+    def __init__(self, error_code):
+        super(Exception, self).__init__(self)
+        self.error_code = error_code
+        self.error_description = error_to_string(error_code)
+
+    def __str__(self):
+        return f'HPCSError: {self.error_description} ({self.error_code})'
 
 
 """
@@ -35,17 +48,16 @@ Note that detection of `Power` and `Pressure` data might be unreliable.
  - 'HPCS_TYPE_CE_VOLTAGE': Electric voltage in the CE system (7),
  - 'HPCS_TYPE_UNKNOWN': Unknown type of data (8)
 """
-HPCS_FileType = {
-    'HPCS_TYPE_CE_ANALOG': 0,
-    'HPCS_TYPE_CE_CCD': 1,
-    'HPCS_TYPE_CE_CURRENT': 2,
-    'HPCS_TYPE_CE_DAD': 3,
-    'HPCS_TYPE_CE_POWER': 4,
-    'HPCS_TYPE_CE_PRESSURE': 5,
-    'HPCS_TYPE_CE_TEMPERATURE': 6,
-    'HPCS_TYPE_CE_VOLTAGE': 7,
-    'HPCS_TYPE_UNKNOWN': 8
-}
+class HPCS_FileType(IntEnum):
+    HPCS_TYPE_CE_ANALOG = 0
+    HPCS_TYPE_CE_CCD = 1
+    HPCS_TYPE_CE_CURRENT = 2
+    HPCS_TYPE_CE_DAD = 3
+    HPCS_TYPE_CE_POWER = 4
+    HPCS_TYPE_CE_PRESSURE = 5
+    HPCS_TYPE_CE_TEMPERATURE = 6
+    HPCS_TYPE_CE_VOLTAGE = 7
+    HPCS_TYPE_UNKNOWN = 8
 
 """
 `HPCS_RetCode` represents the possible status codes returned from libHPCS
@@ -58,15 +70,14 @@ HPCS_FileType = {
  - 'HPCS_E_INCOMPATIBLE_FILE': File type is not compatible with the requested operation (5),
  - 'HPCS_E_NOTIMPL': Function is not implemented (6)
 """
-HPCS_RetCode = {
-    'HPCS_OK': 0,
-    'HPCS_E_NULLPTR': 1,
-    'HPCS_E_CANT_OPEN': 2,
-    'HPCS_E_PARSE_ERROR': 3,
-    'HPCS_E_UNKNOWN_TYPE': 4,
-    'HPCS_E_INCOMPATIBLE_FILE': 5,
-    'HPCS_E_NOTIMPL': 6
-}
+class HPCS_RetCode(IntEnum):
+    HPCS_OK = 0
+    HPCS_E_NULLPTR = 1
+    HPCS_E_CANT_OPEN = 2
+    HPCS_E_PARSE_ERROR = 3
+    HPCS_E_UNKNOWN_TYPE = 4
+    HPCS_E_INCOMPATIBLE_FILE = 5
+    HPCS_E_NOTIMPL = 6
 
 """
 `HPCS_Date` represents a timestamp returned by libHPCS
@@ -78,7 +89,7 @@ HPCS_RetCode = {
  - 'minute': Minute
  - 'second': Second
 """
-class HPCS_Date(Structure):
+class _HPCS_Date(Structure):
     _fields_ = [("year", c_uint32),
                 ("month", c_uint8),
                 ("day", c_uint8),
@@ -94,7 +105,7 @@ class HPCS_Date(Structure):
 
 Both values set to zero indicate invalid or empty data
 """
-class HPCS_Wavelength(Structure):
+class _HPCS_Wavelength(Structure):
     _fields_ = [("wavelength", c_uint16),
                 ("interval", c_uint16)]
 
@@ -104,12 +115,12 @@ class HPCS_Wavelength(Structure):
  - 'time': Time of sampling, in minutes
  - 'value': Recorded value, unit depends on the kind of the signal trace
 """
-class HPCS_TVPair(Structure):
+class _HPCS_TVPair(Structure):
     _fields_ = [("time", c_double),
                 ("value", c_double)]
 
 """
-`HPCS_MeasuredData` contains information about a measurement,
+`_HPCS_MeasuredData` contains information about a measurement,
 containing both the signal trace and the associated metadata.
 
  - 'file_description': Internal file type description. For most LC or CE data this will be "LC DATA FILE"
@@ -121,37 +132,37 @@ containing both the signal trace and the associated metadata.
  - 'cs_rev': Revision of the used ChemStation software
  - 'sampling_rate': Sampling rate of the detector, in Hz. Value is -1 if the sampling rate is unknown
  - 'y_units': Unit of the signal trace
- - 'dad_wavelength_msr': Detection wavelength of the DAD detector. Applicable only for DAD file types. See `HPCS_Wavelength`
- - 'dad_wavelength_ref': Reference wavelength, usually used for noise compensation. Applicable only for DAD file types. See `HPCS_Wavelength`
+ - 'dad_wavelength_msr': Detection wavelength of the DAD detector. Applicable only for DAD file types. See `_HPCS_Wavelength`
+ - 'dad_wavelength_ref': Reference wavelength, usually used for noise compensation. Applicable only for DAD file types. See `_HPCS_Wavelength`
  - 'file_type': Type of the file. See `HPCS_FileType` for possible values
- - 'data': Array of `HPCS_TVPair`s that represent the signal trace
- - 'data_count': Number of `HPCS_TVPair`s in `data`
+ - 'data': Array of `_HPCS_TVPair`s that represent the signal trace
+ - 'data_count': Number of `_HPCS_TVPair`s in `data`
 """
-class HPCS_MeasuredData(Structure):
+class _HPCS_MeasuredData(Structure):
     _fields_ = [("file_description", c_char_p),
                 ("sample_info", c_char_p),
                 ("operator_name", c_char_p),
-                ("date", HPCS_Date),
+                ("date", _HPCS_Date),
                 ("method_name", c_char_p),
                 ("cs_ver", c_char_p),
                 ("cs_rev", c_char_p),
                 ("y_units", c_char_p),
                 ("sampling_rate", c_double),
-                ("dad_wavelength_msr", HPCS_Wavelength),
-                ("dad_wavelength_ref", HPCS_Wavelength),
+                ("dad_wavelength_msr", _HPCS_Wavelength),
+                ("dad_wavelength_ref", _HPCS_Wavelength),
                 ("file_type", c_int),  # Use c_int for enum
-                ("data", POINTER(HPCS_TVPair)),
+                ("data", POINTER(_HPCS_TVPair)),
                 ("data_count", c_size_t)]
 
 """
-`HPCS_MethodInfoBlock` is a key-value pair of information
+`_HPCS_MethodInfoBlock` is a key-value pair of information
 about a measurement method. Method information can be obtained
 from .MTH files.
 
  - 'name': Name of the parameter
  - 'value': Value of the parameter
 """
-class HPCS_MethodInfoBlock(Structure):
+class _HPCS_MethodInfoBlock(Structure):
     _fields_ = [("name", c_char_p),
                 ("value", c_char_p)]
 
@@ -162,29 +173,167 @@ and the length of that array.
  - 'blocks': Array of `HPCS_MethodInfoBlock`s
  - 'count': Length of the array
 """
-class HPCS_MethodInfo(Structure):
-    _fields_ = [("blocks", POINTER(HPCS_MethodInfoBlock)),
+class _HPCS_MethodInfo(Structure):
+    _fields_ = [("blocks", POINTER(_HPCS_MethodInfoBlock)),
                 ("count", c_size_t)]
 
-# Function wrappers
+
+@dataclass(frozen=True)
+class HPCS_Date:
+    year: int
+    month: int
+    day: int
+    hour: int
+    minute: int
+    second: int
+
+
+@dataclass(frozen=True)
+class HPCS_Wavelength:
+    wavelength: int
+    interval: Optional[int]
+
+
+@dataclass(frozen=True)
+class HPCS_MeasuredData:
+    file_description: str
+    sample_info: str
+    operator_name: str
+    date: HPCS_Date
+    method_name: str
+    cs_ver: str
+    cs_rev: str
+    y_units: str
+    sampling_rate: Optional[float]
+    dad_wavelength_msr: Optional[HPCS_Wavelength]
+    dad_wavelength_ref: Optional[HPCS_Wavelength]
+    file_type: HPCS_FileType
+    data: List[Tuple[float, float]]
+
+
+@dataclass(frozen=True)
+class HPCS_MethodInfo:
+    information: Dict[str, str]
+
+
 def wrap_function(lib, funcname, restype, argtypes):
     func = getattr(lib, funcname)
     func.restype = restype
     func.argtypes = argtypes
     return func
 
-# Allocate and free measured data
-alloc_mdata = wrap_function(libhpcs, "hpcs_alloc_mdata", POINTER(HPCS_MeasuredData), [])
-free_mdata = wrap_function(libhpcs, "hpcs_free_mdata", None, [POINTER(HPCS_MeasuredData)])
+# Internal C functions. It is inadvisable to call these directly
+_error_to_string = wrap_function(libhpcs, "hpcs_error_to_string", c_char_p, [c_int])
+_read_mdata = wrap_function(libhpcs, "hpcs_read_mdata", c_int, [c_char_p, POINTER(_HPCS_MeasuredData)])
+_read_mheader = wrap_function(libhpcs, "hpcs_read_mheader", c_int, [c_char_p, POINTER(_HPCS_MeasuredData)])
+_read_minfo = wrap_function(libhpcs, "hpcs_read_minfo", c_int, [c_char_p, POINTER(_HPCS_MethodInfo)])
+_alloc_mdata = wrap_function(libhpcs, "hpcs_alloc_mdata", POINTER(_HPCS_MeasuredData), [])
+_free_mdata = wrap_function(libhpcs, "hpcs_free_mdata", None, [POINTER(_HPCS_MeasuredData)])
+_alloc_minfo = wrap_function(libhpcs, "hpcs_alloc_minfo", POINTER(_HPCS_MethodInfo), [])
+_free_minfo = wrap_function(libhpcs, "hpcs_free_minfo", None, [POINTER(_HPCS_MethodInfo)])
 
-# Allocate and free method information
-alloc_minfo = wrap_function(libhpcs, "hpcs_alloc_minfo", POINTER(HPCS_MethodInfo), [])
-free_minfo = wrap_function(libhpcs, "hpcs_free_minfo", None, [POINTER(HPCS_MethodInfo)])
+def _make_date(raw_date):
+    return HPCS_Date(
+        raw_date.year,
+        raw_date.month,
+        raw_date.day,
+        raw_date.hour,
+        raw_date.minute,
+        raw_date.second
+    )
+
+
+def _make_wavelength(raw_wavelength):
+    if raw_wavelength.wavelength == 0:
+        return None
+
+    wavelength = raw_wavelength.wavelength
+    interval = raw_wavelength.interval if raw_wavelength.interval != 0 else None
+
+    return HPCS_Wavelength(wavelength, interval)
+
+
+def _make_data(raw_data, raw_data_count):
+    data = []
+    for idx in range(0, raw_data_count):
+        v = raw_data[idx]
+        data.append((v.time, v.value))
+    return data
+
+
+def _make_hpcs_measured_data(ptr):
+    detection_wavelength = None
+    reference_wavelength = None
+    if ptr.contents.file_type == HPCS_FileType.HPCS_TYPE_CE_DAD:
+        detection_wavelength = _make_wavelength(ptr.contents.dad_wavelength_msr)
+        reference_wavelength = _make_wavelength(ptr.contents.dad_wavelength_ref)
+
+    data = _make_data(ptr.contents.data, ptr.contents.data_count)
+
+    return HPCS_MeasuredData(
+        ptr.contents.file_description.decode('utf-8'),
+        ptr.contents.sample_info.decode('utf-8'),
+        ptr.contents.operator_name.decode('utf-8'),
+        _make_date(ptr.contents.date),
+        ptr.contents.method_name.decode('utf-8'),
+        ptr.contents.cs_ver.decode('utf-8'),
+        ptr.contents.cs_rev.decode('utf-8'),
+        ptr.contents.y_units.decode('utf-8'),
+        None if ptr.contents.sampling_rate == -1.0 else ptr.contents.sampling_rate,
+        detection_wavelength,
+        reference_wavelength,
+        ptr.contents.file_type,
+        data
+    )
+
+
+def _make_hpcs_method_info(ptr):
+    information = {}
+
+    for idx in range(0, ptr.contents.count):
+        kv = ptr.contents.blocks[idx]
+        information[kv.name.decode('utf-8')] = kv.value.decode('utf-8')
+
+    return HPCS_MethodInfo(information)
+
 
 # Error to string translation
-error_to_string = wrap_function(libhpcs, "hpcs_error_to_string", c_char_p, [c_int])
+def error_to_string(err):
+    return _error_to_string(err).decode('utf-8')
+
 
 # Reading measured data and method information
-read_mdata = wrap_function(libhpcs, "hpcs_read_mdata", c_int, [c_char_p, POINTER(HPCS_MeasuredData)])
-read_mheader = wrap_function(libhpcs, "hpcs_read_mheader", c_int, [c_char_p, POINTER(HPCS_MeasuredData)])
-read_minfo = wrap_function(libhpcs, "hpcs_read_minfo", c_int, [c_char_p, POINTER(HPCS_MethodInfo)])
+def read_mdata(file_path):
+    ptr = _alloc_mdata()
+    ret =  _read_mdata(str(file_path).encode('utf-8'), ptr)
+    if ret != HPCS_RetCode.HPCS_OK:
+        _free_mdata(ptr)
+        raise HPCSError(ret)
+    else:
+        data = _make_hpcs_measured_data(ptr)
+        _free_mdata(ptr)
+        return data
+
+
+def read_mheader(file_path):
+    ptr = _alloc_mdata()
+    ret =  _read_mheader(str(file_path).encode('utf-8'), ptr)
+    if ret != HPCS_RetCode.HPCS_OK:
+        _free_mdata(ptr)
+        raise HPCSError(ret)
+    else:
+        data = _make_hpcs_measured_data(ptr)
+        _free_mdata(ptr)
+        return data
+
+
+def read_minfo(file_path):
+    ptr = _alloc_minfo()
+    ret =  _read_minfo(str(file_path).encode('utf-8'), ptr)
+    if ret != HPCS_RetCode.HPCS_OK:
+        _free_minfo(ptr)
+        raise HPCSError(ret)
+    else:
+        data = _make_hpcs_method_info(ptr)
+        _free_minfo(ptr)
+        return data
